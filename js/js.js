0a544326be44a3f351dcb79af4edb1072677e282
admin <darina.talk@gmail.com>
2019-11-06 15:30:25 +0300

    15

--- js/js.js
+++ js/js.js
@@ -78,6 +78,63 @@ $(function initSlider(){
   });
 });
 
+$(function initSlider(){
+  $('.review1').slick({     
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1500,
+    prevArrow: '.second-line__arrow-left1',
+    nextArrow: '.second-line__arrow-right1',
+  });
+});
+
+$(function initSlider(){
+  $('.review2').slick({     
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1500,
+    prevArrow: '.second-line__arrow-left2',
+    nextArrow: '.second-line__arrow-right2',
+  });
+});
+
+$(function initSlider(){
+  $('.review3').slick({     
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1500,
+    prevArrow: '.second-line__arrow-left3',
+    nextArrow: '.second-line__arrow-right3',
+  });
+});
+
+// Timer
+
+var countDownDate = new Date("Jan 5, 2020 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("min").innerHTML = minutes;
+  document.getElementById("sec").innerHTML = seconds;
+    
+
+}, 1000);
 
 
 
